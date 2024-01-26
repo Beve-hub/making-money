e4a34505884ee90bb8ad21642029630c3886a1ac
Believe <117173080+Beve-hub@users.noreply.github.com>
2024-01-26 12:24:43 +0000

    ertre

--- main/chart.js
+++ main/chart.js
@@ -1,28 +1,42 @@
-// your-chart-script.js
-
+const xValues = ["Direct", "Affilliate", "Sponsored", "Email" ];
+const yValues = [100, 100, 100, 100];
 const barColors = [
-  "#b91d47",
-  "#00aba9",
-  "#2b5797",
-  "#e8c3b9",
-  "#1e7145"
+  "#8F00FF",
+  "#ffff",
+  "#00E0FF",
+  "#55FE66",
+ 
 ];
 
 new Chart("myChart", {
   type: "doughnut",
   data: {
-    labels: ["Italy", "France", "Spain", "USA", "Argentina"],
+    labels: xValues,
     datasets: [{
       backgroundColor: barColors,
-      data: [55, 49, 44, 24, 15]
+      data: yValues
     }]
   },
   options: {
     title: {
       display: true,
-      text: "Today's "
+      text: ""
     }
   }
 });
-
-
+data: [{
+  type: "doughnut",
+  startAngle: 60,
+  //innerRadius: 60,
+  indexLabelFontSize: 17,
+  indexLabel: "{label} - #percent%",
+  toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+  dataPoints: [
+    { y: 67, label: "Inbox" },
+    { y: 28, label: "Archives" },
+    { y: 10, label: "Labels" },
+    { y: 7, label: "Drafts"},
+    { y: 15, label: "Trash"},
+    { y: 6, label: "Spam"}
+  ]
+}]
