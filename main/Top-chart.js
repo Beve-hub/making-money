0041074368e1302bafc4f960119b6a30cfb81d85
Believe <117173080+Beve-hub@users.noreply.github.com>
2024-01-27 16:17:19 +0000

    fgddfgfdfdg

--- main/Top-chart.js
+++ main/Top-chart.js
@@ -5,11 +5,11 @@ new Chart("topChart", {
       labels:[100,200,300,400,500,600,700,800,900,1000],
       datasets: [{
         data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
-        borderColor: "red",
+        borderColor:  "#55FE66",
         fill: false
       },{
         data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
-        borderColor: "green",
+        borderColor:  "#fff",
         fill: false
       }]
     },
