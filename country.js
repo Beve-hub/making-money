d9b06d51e5353fd8d711f31cc882050bcef44f68
Believe <117173080+Beve-hub@users.noreply.github.com>
2024-02-09 03:37:28 +0000

    sfddd

---
+++ country.js
@@ -0,0 +1,14 @@
+const countrySelect = document.getElementById("country");
+
+// Fetch the list of countries from the Restcountries API
+fetch("https://restcountries.com/v3.1/all")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((country) => {
+      const option = document.createElement("option");
+      option.value = country.alpha3Code;
+      option.text = country.name.common;
+      countrySelect.appendChild(option);
+    });
+  })
+  .catch((error) => console.error("Error fetching countries:", error));
